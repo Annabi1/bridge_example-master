f27d4dc49aff295d0a205dc026e4336beda0c2f8
Annabi1 <annabi.mabrouka99@gmail.com>
2022-07-05 11:04:39 +0100

    second commit

--- src/main/java/org/mss/bridge/to/spades/MsBridgeToSpadesApplication.java
+++ src/main/java/org/mss/bridge/to/spades/MsBridgeToSpadesApplication.java
@@ -2,8 +2,6 @@ package org.mss.bridge.to.spades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
